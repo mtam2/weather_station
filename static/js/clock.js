40764f07595b365f78e453fca1ad95b30c1960f1
Maxim Tam <maximtea.cloud@gmail.com>
2018-01-01 18:46:16 -0500

    Initial Commit

---
+++ static/js/clock.js
@@ -0,0 +1,16 @@
+// http://www.unm.edu/~tbeach/IT145/week13/example09.html
+function mytime()
+{
+    var d=new Date();
+    ap="am";
+    h=d.getHours();
+    m=d.getMinutes();
+    s=d.getSeconds();
+    if (h>11) { ap = "pm"; }
+    if (h>12) { h = h-12; }
+    if (h==0) { h = 12; }
+    if (m<10) { m = "0" + m; }
+    if (s<10) { s = "0" + s; }
+    document.getElementById('timehere').innerHTML=h + ":" + m + ":" + s + " " + ap;
+    t=setTimeout('mytime()',500);
+}
